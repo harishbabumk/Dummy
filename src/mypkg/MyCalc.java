f7edcddd447add76ffcaaf20911329dc12b12af6
harishbabumk <harishbabumk@gmail.com>
2024-05-15 21:55:43 +0530

    diff() added

--- src/mypkg/MyCalc.java
+++ src/mypkg/MyCalc.java
@@ -6,9 +6,15 @@ public class MyCalc {
 		return a+b;
 	}
 	
+	int diff(int a, int b)
+	{
+		return a-b;
+	}
+	
 	public static void main (String[] args)
 	{
 		MyCalc calc = new MyCalc();
 		System.out.println("Sum is " + calc.sum(20, 30));
+		System.out.println("Diff is " + calc.diff(30, 20));
 	}
 }
